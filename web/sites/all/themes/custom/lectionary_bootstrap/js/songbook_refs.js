d36a354dfe0f190082fa4875b6f0a463370d7ee7
Claudine Chionh <cc@claudinec.net>
2016-03-29 18:51:28 +1100

    Regular expression to add links to the abbreviation part of songbook references only. Now to add separators in Refine or MySQL and re-import. See #12.

--- web/sites/all/themes/custom/lectionary_bootstrap/js/songbook_refs.js
+++ web/sites/all/themes/custom/lectionary_bootstrap/js/songbook_refs.js
@@ -4,9 +4,8 @@
         /**
          * Adds links to songbook references on song pages.
          */
-
         $( "div.field-name-field-songbook-references > div > div" ).wrap(function() {
-            return "<a href='/songbook/" + $( this ).contents().text() + "'></a>";
+            return "<a href='/songbook/" + $( this ).contents().text().match(/[A-Z].*(?:\s|\-)/) + "'></a>";
         });
     });
 
