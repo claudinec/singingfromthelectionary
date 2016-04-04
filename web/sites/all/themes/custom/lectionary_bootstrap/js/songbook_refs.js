8bc7dca1a1545dd55f20a1e8aec0678b77831b22
Claudine Chionh <cc@claudinec.net>
2016-04-04 17:24:09 +1000

    Change jQuery for songbook references to use clean urls.

--- web/sites/all/themes/custom/lectionary_bootstrap/js/songbook_refs.js
+++ web/sites/all/themes/custom/lectionary_bootstrap/js/songbook_refs.js
@@ -5,8 +5,7 @@
          * Adds links to songbook references on song pages.
          */
         $( "div.field-name-field-songbook-references > div > div" ).wrap(function() {
-            // return "<a href='/songbook/" + $( this ).contents().text().replace(/([A-Z].*)(\s|\-)([0-9].*)/, '$1') + "'></a>";
-            return "<a href='/?q=songbook/" + $( this ).contents().text().replace(/([A-Z].*)(\s|\-)([0-9].*)/, '$1') + "'></a>";
+            return "<a href='/songbook/" + $( this ).contents().text().replace(/([A-Z].*)(\s|\-)([0-9].*)/, '$1') + "'></a>";
         });
     });
 
