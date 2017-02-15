ad29310cf7c1045a2837d50655af0fc9227504c0
Claudine Chionh <info@claudinec.net>
2017-02-15 17:42:14 +1100

    Use term references instead of text fields for scripture reference fields in lectionary sections and songs. Update CSS and JS to match new fields. Resolves #30, #37, #44.

--- web/sites/all/themes/custom/lectionary_bootstrap/js/scripture_links.js
+++ web/sites/all/themes/custom/lectionary_bootstrap/js/scripture_links.js
@@ -4,7 +4,7 @@
         /**
          * Adds links to scripture references on song pages.
          */
-        $( "div.field-name-field-scripture-references > div.col-md-4" ).wrap(function() {
+        $( "div.field-name-field-lectionary-scripture-refer" ).wrap(function() {
             return "<a href='http://bible.oremus.org/?passage=" + $( this ).contents().text() + "'></a>";
         });
     });
